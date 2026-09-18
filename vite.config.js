5de75f84f38b567184e00ad36d81552b5e393afb
Anastasia2372 <272045667+Anastasia2372@users.noreply.github.com>
2026-09-18 18:51:20 -0400

    build: revert to standard vite output for GitHub Pages HTTPS

--- vite.config.js
+++ vite.config.js
@@ -2,42 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
-function stripModuleType() {
-  return {
-    name: 'strip-module-type',
-    apply: 'build',
-    enforce: 'post',
-    transformIndexHtml: {
-      order: 'post',
-      handler(html) {
-        return html
-          .replace(/<script\s+type="module"([^>]*)>/g, '<script$1>')
-          .replace(/<script([^>]*)\stype="module"/g, '<script$1')
-          .replace(/<script([^>]*)\scrossorigin([^>]*)>/g, '<script$1$2>')
-      }
-    }
-  }
-}
-
 export default defineConfig(({ command }) => ({
   base: command === 'build' ? '/dungeon-xunqi/' : '/',
-  plugins: [
-    vue(),
-    viteSingleFile({
-      removeViteModuleLoader: true,
-      useRecommendedBuildConfig: true
-    }),
-    stripModuleType()
-  ],
+  plugins: [vue(), viteSingleFile()],
   build: {
     target: 'es2019',
     cssMinify: true,
-    minify: true,
-    rollupOptions: {
-      output: {
-        format: 'iife',
-        inlineDynamicImports: true
-      }
-    }
+    minify: true
   }
 }))
